2a6f3e53466fb1860233f76f724a15c7bd2acccd
Adrian <alextravc.342@gmail.com>
2024-03-11 21:01:30 -0500

    ADD: search in free controller

--- src/controllers/free.controller.js
+++ src/controllers/free.controller.js
@@ -59,6 +59,8 @@ export const getPetsFree = async (req, res, next) => {
 export const searchPets = async (req, res, next) => {
   try {
     const searchTerm = req.query.search;
+    const page = parseInt(req.query.page) || 1; // La página actual, por defecto es 1
+    const pageSize = parseInt(req.query.pageSize) || 10; // El número de registros por página, por defecto es 10
 
     const pets = await prisma.mascota.findMany({
       where: {
@@ -93,6 +95,8 @@ export const searchPets = async (req, res, next) => {
           },
         ],
       },
+      skip: (page - 1) * pageSize,
+      take: pageSize,
       include: {
         Responsable: {
           include: {
@@ -107,7 +111,50 @@ export const searchPets = async (req, res, next) => {
       },
     });
 
-    res.status(200).json({ ok: true, pets });
+    const totalPets = await prisma.mascota.count({
+      where: {
+        OR: [
+          {
+            nombre_mascota: {
+              contains: searchTerm,
+            },
+          },
+          {
+            codigo_chip: {
+              contains: searchTerm,
+            },
+          },
+          {
+            Responsable: {
+              user: {
+                nombre: {
+                  contains: searchTerm,
+                },
+              },
+            },
+          },
+          {
+            Tutor: {
+              user: {
+                nombre: {
+                  contains: searchTerm,
+                },
+              },
+            },
+          },
+        ],
+      },
+    }); // Obtiene el total de mascotas que coinciden con el término de búsqueda
+    const totalPages = Math.ceil(totalPets / pageSize); // Calcula el total de páginas
+
+    res.status(200).json({
+      ok: true,
+      count: totalPets,
+      next: page < totalPages ? page + 1 : null,
+      previous: page > 1 ? page - 1 : null,
+      numero_paginas: totalPages,
+      data: pets,
+    });
   } catch (error) {
     next(error);
   }
